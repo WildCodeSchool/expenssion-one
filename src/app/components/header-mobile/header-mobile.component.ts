6f6c2214f78144548935cd326350b6fda81bb2ad
boubseb <sebpel62@gmail.com>
2024-06-11 15:18:56 +0200

    clean

--- src/app/components/header-mobile/header-mobile.component.ts
+++ src/app/components/header-mobile/header-mobile.component.ts
@@ -1,21 +1,19 @@
-import { Component, inject } from '@angular/core';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header-mobile',
   standalone: true,
-  imports: [RouterOutlet, MatSidenavModule, MatIconModule, CommonModule],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './header-mobile.component.html',
   styleUrl: './header-mobile.component.scss'
 })
 export class HeaderMobileComponent {
 
-  isMobileSidenavOpen:boolean=false
+  isMobileSideNavOpen:boolean=false
 
-  toogle(){
-    this.isMobileSidenavOpen=!this.isMobileSidenavOpen;
+  toggle(){
+    this.isMobileSideNavOpen=!this.isMobileSideNavOpen;
 
   }
 
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,35 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatExpansionModule, CommonModule,RouterLink],
+  imports: [CommonModule,RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  isShowJeu: boolean = false;
-  isShowGlossaire: boolean = false;
-  isSubMenuHover: boolean = false;
-  isLeftNavHover: boolean = false;
 
 
-
-
-  changeGlossaire() {
-    this.isShowGlossaire = true;
-    if (this.isShowJeu == true) {
-      this.isShowJeu = false
-    }
-  }
-
-  changeJeu() {
-    this.isShowJeu = true;
-    if (this.isShowGlossaire == true) {
-      this.isShowGlossaire = false
-    }
-
-  }
 }
