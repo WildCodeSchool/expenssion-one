fc7a16bbffa5b10ca36ea7858c168e09b040fd54
Kileana27 <blond.camillelm@gmail.com>
2024-06-12 17:44:12 +0200

    added getLocations method

--- src/app/service/api-data.service.ts
+++ src/app/service/api-data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { Specialization } from '../model/specialization/specialization';
+import { Locations } from '../model/locations/locations';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 @Injectable({
@@ -11,5 +12,9 @@ export class ApiDataService {
 
   getSpecialization():Observable<Specialization>{
     return this.http.get<Specialization>('assets/data/specialization.json')
+  }
+
+  getLocations():Observable<Locations>{
+    return this.http.get<Locations>('assets/data/continents.json')
   }
 }
