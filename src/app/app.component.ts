4d787091c9188032c35f4e24a5f1a289b2eb536f
Cyril <c.concas.ext@axione.fr>
2024-06-12 13:43:24 +0200

    Correction PopUp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { HeaderMobileComponent } from './components/header-mobile/header-mobile.component';
 import { FooterComponent } from './components/footer/footer.component';
-import {MatDialog, MatDialogModule} from '@angular/material/dialog';
-import {MatButtonModule} from '@angular/material/button';
-import { PopupComponent } from './components/pop-up-connexion/pop-up-connexion.component';
+
 
 
 @Component({
@@ -20,8 +18,10 @@ export class AppComponent{
   title = 'Expenssion One';
   isMobile: boolean = false;
 
+  
+
   private breakpoints = {
-    mobile: '(max-width: 768px)',
+    mobile: '(max-width: 768px)'
   };
 
 
@@ -29,17 +29,6 @@ export class AppComponent{
 
   ngOnInit() {
     this.breakpointObserver.observe(Object.values(this.breakpoints)).subscribe((state: BreakpointState) => {
-      this.isMobile = state.breakpoints[this.breakpoints.mobile];
-     
-  constructor(public dialog: MatDialog) {}
-  openDialog() {
-    const dialogRef = this.dialog.open(PopupComponent, {
-      width: '254px',
-      height: '351px'
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
+      this.isMobile = state.breakpoints[this.breakpoints.mobile]});
+    }
 }
