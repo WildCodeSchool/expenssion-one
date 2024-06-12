800866c20e94d4675c1fd9673470f8bbfc419e43
Kileana27 <blond.camillelm@gmail.com>
2024-06-12 17:25:46 +0200

    Created location-page component

---
+++ src/app/pages/location-page/location-page/location-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './location-page.component.html',
+  styleUrl: './location-page.component.scss'
+})
+export class LocationPageComponent {
+
+}
