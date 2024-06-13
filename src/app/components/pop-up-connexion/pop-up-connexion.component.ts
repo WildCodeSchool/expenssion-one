bf1625af43a9b3fa36edc7bffb267958755209db
AnassAmh <anass94320@gmail.com>
2024-06-13 14:05:23 +0200

    ajout du lien de redirection de la page connexion vers la page inscription

--- src/app/components/pop-up-connexion/pop-up-connexion.component.ts
+++ src/app/components/pop-up-connexion/pop-up-connexion.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import {MatDialog, MatDialogModule} from '@angular/material/dialog';
-import {MatButtonModule} from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import { Login } from '../../model/login.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-popup',
@@ -12,11 +13,15 @@ import { Login } from '../../model/login.model';
   styleUrl: './pop-up-connexion.component.scss'
 })
 export class PopupComponent {
-  login:Login=new Login("","");
-  url:String="../../../assets/background_pictures/logo_blop_10001.png";
+  login: Login = new Login("", "");
+  url: String = "../../../assets/background_pictures/logo_blop_10001.png";
 
-  displayData()
-  {
-    console.log(this.login.login  + " " + this.login.password);
+  constructor(private router: Router) { }
+
+  redirectToRegister() {
+    this.router.navigate(['/register']);
+  }
+  displayData() {
+    console.log(this.login.login + " " + this.login.password);
   }
 }
