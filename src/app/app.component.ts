8721758ec53e472e600df24512b697398454384a
Kileana27 <blond.camillelm@gmail.com>
2024-06-13 17:54:31 +0200

    Modifieed app component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { HeaderMobileComponent } from './components/header-mobile/header-mobile.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { ApiDataService } from './service/api-data.service';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -14,20 +14,16 @@ import { ApiDataService } from './service/api-data.service';
 })
 
 export class AppComponent{
+  title = 'Expenssion One';
+  private breakpoints = {
+    mobile: '(max-width: 768px)',
+}
 
-  apiDataService=inject(ApiDataService)
   breakpointObserver = inject(BreakpointObserver)
+  isMobile: boolean = false;
 
   ngOnInit() {
     this.breakpointObserver.observe(Object.values(this.breakpoints)).subscribe((state: BreakpointState) => {
-      this.isMobile = state.breakpoints[this.breakpoints.mobile];
-     
-      this.apiDataService.getSpecialization().subscribe(x=>console.log(x))
-      title = 'Expenssion One';
-      isMobile: boolean = false;
-    
-      private breakpoints = {
-        mobile: '(max-width: 768px)',
-    });
+      this.isMobile = state.breakpoints[this.breakpoints.mobile]});
   }
 }
