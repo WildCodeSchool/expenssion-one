cbb7bf9ed69814ca94be1a539c0032c8f1fb4bdf
Kileana27 <blond.camillelm@gmail.com>
2024-06-18 14:06:29 +0200

    Created rules-page component

---
+++ src/app/pages/rules-page/rules-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rules-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './rules-page.component.html',
+  styleUrl: './rules-page.component.scss'
+})
+export class RulesPageComponent {
+
+}
