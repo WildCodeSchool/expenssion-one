96f5688e5b5e8622a61afd1be3e5ebe3427bd886
AnassAmh <anass94320@gmail.com>
2024-06-18 18:06:10 +0200

    News-page

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss'] // Correction ici
+  styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent {
   @Input() slides: { image: string, title: string, description: string }[] = [];
