525e6c394f71ec1067b88be81a6b97d5362e63db
AnassAmh <anass94320@gmail.com>
2024-06-19 19:14:17 +0200

    modif

--- src/app/news-page/news-page.component.ts
+++ src/app/news-page/news-page.component.ts
@@ -3,15 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { CarouselComponent } from '../carousel/carousel.component';
+import { HeaderComponent } from '../components/header/header.component';
+import { FooterComponent } from '../components/footer/footer.component';
 @Component({
   selector: 'app-news-page',
   standalone: true,
-  imports: [CommonModule, CarouselComponent],
+  imports: [CommonModule, CarouselComponent, HeaderComponent, FooterComponent],
   templateUrl: './news-page.component.html',
   styleUrls: ['./news-page.component.scss']
 })
 export class NewsPageComponent {
-  private apiUrl = 'http://your-api-url.com/news';
+  private apiUrl = '';
   slides = [
     { image: 'assets/news1.jpg', title: 'News Title 1', description: 'Lorem ipsum dolor sit amet. Sit autem nemo in minus quas nam itaque minima sed dicta atque.' },
     { image: 'assets/news2.jpg', title: 'News Title 2', description: 'Lorem ipsum dolor sit amet. Sit autem nemo in minus quas nam itaque minima sed dicta atque.' },
