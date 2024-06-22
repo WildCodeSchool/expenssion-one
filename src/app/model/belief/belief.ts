8e5dd5b03cd1e4173566362059dabb4d74bfb43c
Cyril <c.concas.ext@axione.fr>
2024-06-22 02:14:05 +0200

    Belief model

--- src/app/model/belief/belief.ts
+++ src/app/model/belief/belief.ts
@@ -1,1 +1,21 @@
-export class Belief {}
+export class Divinity {
+    name!:string;
+    title!:string;
+    gender!:string;
+    effet!:string;
+    strength!:number;
+    dexterity!:number;
+    constitution!:number;
+    intelligence!:number;
+    wisdom!:number;
+    charisma!:number;
+    armor!:number;
+    addiction!:number;
+    speed!:number;
+    perception!:number;
+}
+export class Belief {
+    name!:string;
+    description!:string[];
+    divinities!:Divinity[];
+}
