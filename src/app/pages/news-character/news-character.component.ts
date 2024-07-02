13be87e1bfab0deeae2964051e3c5d2739c79085
AnassAmh <anass94320@gmail.com>
2024-07-02 22:38:53 +0200

    modif du responsive

--- src/app/pages/news-character/news-character.component.ts
+++ src/app/pages/news-character/news-character.component.ts
@@ -9,18 +9,16 @@ export class NewsCharacterComponent {
   @ViewChild('slotsWrapper', { static: true }) slotsWrapper!: ElementRef;
 
   scroll(direction: string) {
-    const scrollAmount = 100;
-    if (direction === 'up') {
-      this.slotsWrapper.nativeElement.scrollBy({
-        top: -scrollAmount,
-        behavior: 'smooth'
-      });
-    } else if (direction === 'down') {
-      this.slotsWrapper.nativeElement.scrollBy({
-        top: scrollAmount,
-        behavior: 'smooth'
-      });
+    const scrollAmount = 100; // Ajustez cette valeur selon vos besoins
+    if (this.slotsWrapper && this.slotsWrapper.nativeElement) {
+      const { nativeElement } = this.slotsWrapper;
+      if (direction === 'up') {
+        nativeElement.scrollTop -= scrollAmount;
+      } else if (direction === 'down') {
+        nativeElement.scrollTop += scrollAmount;
+      }
     }
   }
 }
+
 
