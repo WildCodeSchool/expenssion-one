b68515cc48e249571b6ac62ac8643045ef5c82ac
Kileana27 <blond.camillelm@gmail.com>
2024-07-04 11:51:27 +0200

    Added interactive map

--- src/app/pages/location-selection-page/location-selection-page.component.ts
+++ src/app/pages/location-selection-page/location-selection-page.component.ts
@@ -1,12 +1,23 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-location-selection-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './location-selection-page.component.html',
   styleUrl: './location-selection-page.component.scss'
 })
 export class LocationSelectionPageComponent {
 
+  constructor(private location: Location) { }
+
+  goBack() {
+    this.location.back();
+  }
+
+  onMouseEnter() {
+    console.log("mouse enter");
+  }
 }
