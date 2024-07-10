9389ab408253f17c82fda56c040aa70bcfd7bbd6
Vivi Valienne <valiendanse@gmail.com>
2024-07-10 13:10:41 +0200

    Merged and resolved conflict

--- src/app/pages/secret-selection-page/secret-selection-page.component.ts
+++ src/app/pages/secret-selection-page/secret-selection-page.component.ts
@@ -6,52 +6,48 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { Secret } from '../../model/secret/secret';
 import { SecretService } from '../../service/secrect/secret.service';
 
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { Router } from '@angular/router';
-
-
-
-
-
 
 @Component({
   selector: 'app-secret-selection-page',
   standalone: true,
-  imports: [MatTooltipModule,CommonModule,ReactiveFormsModule],
+  imports: [MatTooltipModule, CommonModule, ReactiveFormsModule],
   templateUrl: './secret-selection-page.component.html',
-  styleUrl: './secret-selection-page.component.scss'
+  styleUrl: './secret-selection-page.component.scss',
 })
 export class SecretSelectionPageComponent {
-  secretService=inject(SecretService)
-  isGenerate:boolean=false
-  isReset:boolean=false
-  annecdoticSecret:Secret=new Secret(0,"");
-  primordialSecret:Secret=new Secret(1,"")
-  router=inject(Router)
+  secretService = inject(SecretService);
+  isGenerate: boolean = false;
+  isReset: boolean = false;
+  annecdoticSecret: Secret = new Secret(0, '');
+  primordialSecret: Secret = new Secret(1, '');
+  router = inject(Router);
 
-  initializeSecrets(){
-    this.secretService.getRandomAnnecdoticSecret().subscribe(x=>{this.annecdoticSecret=x
-    })
-    this.secretService.getRandomPrimordialSecret().subscribe(x=>{this.primordialSecret=x
-    })
-    this.isGenerate=true
+  initializeSecrets() {
+    this.secretService.getRandomAnnecdoticSecret().subscribe((x) => {
+      this.annecdoticSecret = x;
+    });
+    this.secretService.getRandomPrimordialSecret().subscribe((x) => {
+      this.primordialSecret = x;
+    });
+    this.isGenerate = true;
   }
-  resetPrimordialSecret(){
-        this.secretService.getRandomPrimordialSecret().subscribe(x=>{this.primordialSecret=x
-    })
+  resetPrimordialSecret() {
+    this.secretService.getRandomPrimordialSecret().subscribe((x) => {
+      this.primordialSecret = x;
+    });
 
-    this.isReset=true
-    
-  }
-
-  resetSecondarySecret(){
-   this.secretService.getRandomAnnecdoticSecret().subscribe(x=>{this.annecdoticSecret=x
-    })
-    this.isReset=true
+    this.isReset = true;
   }
 
-  saveCharacter(){
-    this.router.navigateByUrl('/inscription')
+  resetSecondarySecret() {
+    this.secretService.getRandomAnnecdoticSecret().subscribe((x) => {
+      this.annecdoticSecret = x;
+    });
+    this.isReset = true;
   }
 
+  saveCharacter() {
+    this.router.navigateByUrl('/inscription');
+  }
 }
